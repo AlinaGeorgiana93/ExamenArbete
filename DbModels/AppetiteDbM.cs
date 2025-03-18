0a44c853d698de395376ed36e3de5a514848b8cf
parisaaz <parisa@phantoms.se>
2025-03-18 13:52:04 +0100

    AppetiteDbM

--- DbModels/AppetiteDbM.cs
+++ DbModels/AppetiteDbM.cs
@@ -9,45 +9,29 @@ using Models.DTO;
 
 namespace DbModels;
 [Table("Appetites", Schema = "supusr")]
-public class AppetiteDbM : Appetite//, ISeed<AppetiteDbM>
+public class AppetiteDbM : Appetite // ISeed<AppetiteDbM>
 {
     [Key]
     public override Guid AppetiteId { get; set; }
 
+
+
+
     #region adding more readability to an enum type in the database
-    public virtual string strKind
+    public virtual string strAppetiteLevel
     {
         get => AppetiteLevel.ToString();
-        set { }  //set is needed by EFC to include in the database, so I make it to do nothing
+        set { } 
     }
-    public virtual string strMood
+    
+    public virtual string strDate
     {
         get => Date.ToString();
-        set { } //set is needed by EFC to include in the database, so I make it to do nothing
+        set { } 
     }
-    #endregion
-    
-    // [NotMapped]
-    // public override IPatient Patient { get => PatientDbM; set => throw new NotImplementedException(); }
+     #endregion
 
-    // [JsonIgnore]
-    // [Required]
-    // public  PatientDbM PatientDbM { get; set; }
-
-    [NotMapped]
-    public override IGraph Graph { get => GraphDbM; set => throw new NotImplementedException(); }
-
-    [JsonIgnore]
-    [Required]
-    public  GraphDbM? GraphDbM { get; set; }
-
-    // public override AppetiteDbM Seed (SeedGenerator _seeder)
-    // {
-    //     base.Seed (_seeder);
-    //     return this;
-    // }
-
-    public AppetiteDbM UpdateFromDTO(AppetiteCuDto org)
+   public AppetiteDbM UpdateFromDTO(AppetiteCuDto org)
     {
         if (org == null) return null;
 
@@ -60,10 +44,23 @@ public class AppetiteDbM : Appetite//, ISeed<AppetiteDbM>
         return this;
     }
 
+
+    /*  [NotMapped]
+    public override IPatient Patient { get => PatientDbM; set => throw new NotImplementedException(); }
+
+    [JsonIgnore]
+    [Required]
+    public  PatientDbM PatientDbM { get; set; }
+ */
+
     public AppetiteDbM() { }
     public AppetiteDbM(AppetiteCuDto org)
     {
         AppetiteId = Guid.NewGuid();
         UpdateFromDTO(org);
     }
+
+  
+
+    
 }
