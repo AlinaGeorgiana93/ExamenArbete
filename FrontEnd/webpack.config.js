e83bdb21bdef98981d6afb27abc50b89935445ec
AlinaGeorgiana93 <georgiana.alina93@yahoo.com>
2025-03-19 11:11:17 +0100

    frontend install modules

---
+++ FrontEnd/webpack.config.js
@@ -0,0 +1,38 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js', // Your main JS file
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+    }),
+  ],
+  devServer: {
+    static: {
+      directory: path.resolve(__dirname, 'dist'), // Serve static files from dist
+    },
+    port: 3000,
+    open: true, // Automatically open the browser
+    hot: true, // Enable Hot Module Replacement
+  },
+};
