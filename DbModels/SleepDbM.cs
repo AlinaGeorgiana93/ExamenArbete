8117b0cf94c2493ab01b0516d5c697213a36dac8
NagihanCifo <nagihan@phantoms.se>
2025-03-24 10:09:58 +0100

    New Sleep Controller, Delete the old one

--- DbModels/SleepDbM.cs
+++ DbModels/SleepDbM.cs
@@ -9,10 +9,13 @@ using Models.DTO;
 
 namespace DbModels;
 [Table("Sleeps", Schema = "supusr")]
-public class SleepDbM : Sleep//, ISeed<SleepDbM>
+public class SleepDbM : Sleep
 {
     [Key]
     public override Guid SleepId { get; set; }
+
+
+
 
     #region adding more readability to an enum type in the database
     public virtual string strSleepLevel
@@ -20,27 +23,15 @@ public class SleepDbM : Sleep//, ISeed<SleepDbM>
         get => SleepLevel.ToString();
         set { } 
     }
+    
     public virtual string strDate
     {
         get => Date.ToString();
         set { } 
     }
     #endregion
-    
-   /*  [NotMapped]
-    public override IPatient Patient { get => PatientDbM; set => throw new NotImplementedException(); }
 
-    [JsonIgnore]
-    [Required]
-    public  PatientDbM PatientDbM { get; set; }
- */
-    // public override SleepDbM Seed (SeedGenerator _seeder)
-    // {
-    //     base.Seed (_seeder);
-    //     return this;
-    // }
-
-    public SleepDbM UpdateFromDTO(SleepCuDto org)
+   public SleepDbM UpdateFromDTO(SleepCuDto org)
     {
         if (org == null) return null;
 
@@ -53,10 +44,31 @@ public class SleepDbM : Sleep//, ISeed<SleepDbM>
         return this;
     }
 
+
+    [NotMapped]
+    public override IPatient Patient { get => PatientDbM; set => throw new NotImplementedException(); }
+
+    [JsonIgnore]
+    [Required]
+    public  PatientDbM PatientDbM { get; set; }
+
+    [NotMapped]
+    public override IGraph Graph { get => GraphDbM; set => throw new NotImplementedException(); }
+
+    [JsonIgnore]
+    [Required]
+    public  GraphDbM GraphDbM { get; set; }
+ 
+ 
+
     public SleepDbM() { }
     public SleepDbM(SleepCuDto org)
     {
         SleepId = Guid.NewGuid();
         UpdateFromDTO(org);
     }
+
+  
+
+    
 }
