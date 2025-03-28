75a52b5cbd236656d8c9341b1316ffe5c4ba7422
parisaaz <parisa@phantoms.se>
2025-03-28 15:06:08 +0100

    GraphRepos,Graph,IGraph

--- Models/Graph.cs
+++ Models/Graph.cs
@@ -10,9 +10,9 @@ public class Graph : IGraph
 
 
     public virtual IPatient Patient { get; set; } // Navigation property without Foreign Key
-    public virtual List<IActivity> Activities { get; set; }
-    public virtual List<IAppetite> Appetites { get; set; }
-    public virtual List<IMood> Moods { get; set; }
-    public virtual List<ISleep> Sleeps { get; set; }
+    // public virtual List<IActivity> Activities { get; set; }
+    // public virtual List<IAppetite> Appetites { get; set; }
+    // public virtual List<IMood> Moods { get; set; }
+    // public virtual List<ISleep> Sleeps { get; set; }
 
 }
