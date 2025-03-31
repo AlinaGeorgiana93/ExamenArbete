212849b34aea97479a2a2021a2f106a06c6e6a71
parisaaz <parisa@phantoms.se>
2025-03-31 13:39:37 +0200

    Added connection of Appetite and AppetiteLevel

--- Models/Appetite.cs
+++ Models/Appetite.cs
@@ -7,15 +7,17 @@ namespace Models
     {
 
         public virtual Guid AppetiteId { get; set; }
-        public virtual AppetiteLevel AppetiteLevel { get; set; } // e.g., Low, Medium, High
+        //public virtual AppetiteLevel AppetiteLevel { get; set; } // e.g., Low, Medium, High
         public virtual DateTime Date { get; set; }
         public virtual DayOfWeek Day { get; set; }
         public virtual string Notes { get; set; } // Additional notes about the appetite
+
+        public virtual List<IAppetiteLevel> AppetiteLevels { get; set; }
 
         // Navigation property to Patient
         public virtual IPatient Patient { get; set; }
 
         public virtual IGraph Graph { get; set; }
-
+        
     }
 }
