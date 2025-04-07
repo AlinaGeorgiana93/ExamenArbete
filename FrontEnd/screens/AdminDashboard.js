0c2d26e3a64a24d40c3f769ea6ca0f0b94301f52
AlinaGeorgiana93 <georgiana.alina93@yahoo.com>
2025-04-07 12:39:39 +0200

    install of packages

---
+++ FrontEnd/screens/AdminDashboard.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+import LinearGradient from 'react-native-linear-gradient';
+import '../src/index.css'
+
+
+
---
+++ FrontEnd/screens/StartPage.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+import LinearGradient from 'react-native-linear-gradient';
+
+
+
+
+<LinearGradient
+  colors={['#a0e9fd', '#00d4ff']}
+  start={{ x: 0, y: 0 }}
+  end={{ x: 1, y: 1 }}
+  style={styles.container}
+>
+  <Text style={styles.text}>Turquoise Vibes 🌊</Text>
+</LinearGradient>
